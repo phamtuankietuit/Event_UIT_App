a275dddd6e50f8f0a91f1063217d09bb6326cb45
Quan Ngo <ngotrungquan1412@gmail.com>
2024-06-29 08:11:19 +0700

    ExportFile

--- rn/app/components/Event_Item/index.jsx
+++ rn/app/components/Event_Item/index.jsx
@@ -1,15 +1,28 @@
 import React from "react"
-import { Text, TouchableOpacity, View, Image } from "react-native"
+import { Text, TouchableOpacity, View, Image, Linking } from "react-native"
 import { format } from "date-fns"
 import logo from "../../../assets/images/anhtruong.jpg"
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
+import { faEye, faEyeSlash, faFileExport } from "@fortawesome/free-solid-svg-icons"
 import { useRouter, Link } from "expo-router"
-
+import * as EventServices from '../../apiServices/eventServices'
+import dayjs from "dayjs"
 function Event_Item({ item, deleteItem, deleteButton }) {
   const router = useRouter()
 
-  // console.log(item.id)
+  const exportFile = async () => {
+    const response = await EventServices.getCheckinFile({
+      EventId: item.id
+    }).catch((error) => {
+      showToastWithGravity("Có lỗi xảy ra")
+      console.log(error);
+    })
+
+    if (response) {
+      // Xử lý nếu response trả về
+      Linking.openURL(response.fileUrl).catch(err => console.error("Couldn't load page", err));
+    }
+  }
 
   return (
     <Link
@@ -32,7 +45,7 @@ function Event_Item({ item, deleteItem, deleteButton }) {
         elevation: 4,
       }}
     >
-      <View className='h-[90px] w-full flex-row items-center'>
+      <View className='h-[90px] w-[100%] flex-row items-center'>
         <Image
           source={item?.images ? { uri: item?.images[0].imageUrl } : logo}
           className='mx-2 h-[80px] w-[80px] rounded-[3px] bg-white'
@@ -42,17 +55,25 @@ function Event_Item({ item, deleteItem, deleteButton }) {
           <Text numberOfLines={2} className='mb-3 font-semibold text-xs'>
             {item.title}
           </Text>
-          {item.isPublished === true ? (
-            <Text className='text-xs'>
-              Ngày công bố : {format(item?.publishedDate, "HH:mm - dd/MM/yyyy")}
-            </Text>
-          ) : (
-            <Text className='text-xs'>
-              Thời gian bắt đầu :{" "}
-              {format(item?.startDate, "HH:mm - dd/MM/yyyy")}
-            </Text>
-          )}
+          <View className='flex-row justify-between w-full'>
+            {item.isPublished === true ? (
+              <Text className='text-xs'>
+                Ngày công bố : {dayjs(item?.publishedDate).format("HH:mm - DD/MM/YYYY")}
+              </Text>
+            ) : (
+              <Text className='text-xs'>
+                Thời gian bắt đầu :{" "}
+                {dayjs(item?.startDate).format("HH:mm - DD/MM/YYYY")}
+              </Text>
+            )}
+            <TouchableOpacity className=' flex-1 flex items-center justify-center' onPress={() => exportFile()}>
+              <FontAwesomeIcon
+                icon={faFileExport}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
+
         {/* {deleteButton && (
           <TouchableOpacity className=' mr-2 flex-1 flex items-center justify-center'>
             <View className='rounded-full bg-orange-400 p-3'>
